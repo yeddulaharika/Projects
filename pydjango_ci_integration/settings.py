682c1c54d863c4e04976fa08f23ad9e9d326462b
vibhor-07 <35040707+vibhor-07@users.noreply.github.com>
2022-09-13 17:14:13 +0530

    Update settings.py

--- pydjango_ci_integration/settings.py
+++ pydjango_ci_integration/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+from pathlib import Path
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -95,7 +97,7 @@ WSGI_APPLICATION = 'pydjango_ci_integration.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR \ 'db.sqlite3',
+        'NAME': BASE_DIR // 'db.sqlite3',
     }
 }
 
