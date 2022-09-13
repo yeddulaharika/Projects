1f1a1d274f25a0ccf5d3d3d79308273d052ddfce
vibhor-07 <35040707+vibhor-07@users.noreply.github.com>
2022-09-13 17:16:17 +0530

    Update settings.py

--- pydjango_ci_integration/settings.py
+++ pydjango_ci_integration/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'pydjango_ci_integration.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR // 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
